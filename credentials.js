7906eff36031b67e247c64e80d2185e9ede67f51
Peter Socha <psocha@uwaterloo.ca>
2016-04-23 13:29:10 -0700

    Gitignore and filler credentials file

---
+++ credentials.js
@@ -0,0 +1,6 @@
+exports.credentials = {
+  'consumer_secret':     'NONE_OF_YOUR_BUSINESS',
+  'consumer_key':        'NONE_OF_YOUR_BUSINESS',
+  'access_token':        'NONE_OF_YOUR_BUSINESS',
+  'access_token_secret': 'NONE_OF_YOUR_BUSINESS'
+};
