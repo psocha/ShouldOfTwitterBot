31cb56ee667c10834d5b110d316650f8b37e6ba5
Peter Socha <psocha@uwaterloo.ca>
2016-04-25 20:51:15 -0700

    Tweeting and repeating

--- bot.js
+++ bot.js
@@ -3,16 +3,32 @@ var Twit = require('twit');
 
 var Bot = new Twit(Credentials.credentials);
 
-Bot.get('search/tweets', { q: '\"should of\"', count: 1, lang: 'en' }, function(err, data, response) {
-  if (err) {
-	  console.log('Error while retrieving tweet:', err);
-	  return;
-  }
-  
-  var tweet = data['statuses'][0];
-  
-  var tweetText = tweet['text'];
-  var tweeterScreenName = tweet['user']['screen_name'];
-  
-  console.log(tweeterScreenName, ' wrote \"', tweetText, '\"');
-});
+var shameUser = function(screenName) {
+  var tweet = '@' + screenName + ': You should have written \"should have\", not \"should of\".';
+  console.log('Tweeting:', tweet);
+
+  Bot.post('statuses/update', { status: tweet }, function(err, data, response) {
+    if (err) {
+      console.log('Error while sending tweet:', err);
+    }
+  });
+}
+
+var retrieveMatchingTweet = function() {
+  Bot.get('search/tweets', { q: '\"should of\"', count: 1, lang: 'en' }, function(err, data, response) {
+    if (err) {
+      console.log('Error while retrieving tweet:', err);
+      return;
+    }
+
+    try {
+      var screenName = data['statuses'][0]['user']['screen_name'];
+      shameUser(screenName);
+    } catch(e) {
+      console.log('Exception while parsing or tweeting:', e);
+    }
+  });
+}
+
+retrieveMatchingTweet();
+setInterval(retrieveMatchingTweet, 15 * 60 * 1000);
